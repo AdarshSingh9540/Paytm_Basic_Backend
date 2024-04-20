8ac8ba22e69ecb369b895bda5a2d68271b86f774
Adarsh <adarsh9540984202@gmail.com>
2024-04-20 20:16:48 +0530

    remove comment

--- db/db.js
+++ db/db.js
@@ -11,7 +11,7 @@ const userSchema = new mongoose.Schema({
 
 const accountSchema = new mongoose.Schema({
     userId: {
-        type: mongoose.Schema.Types.ObjectId, // Reference to User model
+        type: mongoose.Schema.Types.ObjectId, 
         ref: 'User',
         required: true
     },
